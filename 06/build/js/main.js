af8eadf2ff56bf208edc03c2a93d786e77e93361
Will D <diweizhi@gmail.com>
2025-08-11 14:52:04 +1000

    06

---
+++ 06/build/js/main.js
@@ -0,0 +1,107 @@
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: string;
+//   constructor(name: string, music: string, age: number, lang: string) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+// }
+class Coder {
+    name;
+    music;
+    age;
+    lang;
+    secondLang;
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+// console.log(Dave.age);
+// console.log(Dave.lang);
+class WebDev extends Coder {
+    computer;
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+class Guitarist {
+    name;
+    instrument;
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+class Peeps {
+    name;
+    static count = 0;
+    static getCount() {
+        return Peeps.count;
+    }
+    id;
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Amy.id);
+console.log(Steve.id);
+console.log(John.id);
+console.log(Peeps.count);
+class Bands {
+    dataState;
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+MyBands.data = ['Van Halen', 5101];
+export {};
+//# sourceMappingURL=main.js.map
