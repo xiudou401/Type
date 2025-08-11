bbf1f3cb4217249fce1fbe5e121dbcb580ace810
Will D <diweizhi@gmail.com>
2025-08-11 10:33:34 +1000

    untracked

---
+++ 04/build/js/main.js
@@ -0,0 +1,69 @@
+//Type Aliases
+// interface PostId = stringOrNumber
+let myName;
+// myName = 'Eason';
+let userName;
+// userName = 'Qiu';
+//functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello');
+logMsg(add(2, 3));
+// logMsg(add('a', 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+const addAll = (a, b, c) => {
+    //type guard
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+// default param value
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+//Rest Parameters
+// const total = (...nums: number[]): number => {
+//   return nums.reduce((prev, curr) => prev + curr);
+// };
+// logMsg(total(1, 2, 3, 4));
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(10, 2, 3));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+const numberOrString = (value) => {
+    if (typeof value === 'string')
+        return 'string';
+    if (typeof value === 'number')
+        return 'number';
+    return createError('This could never happen');
+};
+export {};
+//# sourceMappingURL=main.js.map
---
+++ 04/src/main.ts
@@ -0,0 +1,103 @@
+//Type Aliases
+
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+// type Guitarist = {
+//   name?: string;
+//   active: boolean;
+//   albums: (string | number)[];
+// };
+
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: stringOrNumberArray;
+};
+
+type UserId = stringOrNumber;
+
+// interface PostId = stringOrNumber
+
+let myName: 'Will';
+// myName = 'Eason';
+
+let userName: 'Will' | 'Eason' | 'Nancy';
+// userName = 'Qiu';
+
+//functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg('Hello');
+logMsg(add(2, 3));
+// logMsg(add('a', 3));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+const addAll = (a: number, b: number, c?: number): number => {
+  //type guard
+  if (typeof c !== 'undefined') {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// default param value
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+
+//Rest Parameters
+// const total = (...nums: number[]): number => {
+//   return nums.reduce((prev, curr) => prev + curr);
+// };
+
+// logMsg(total(1, 2, 3, 4));
+
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((prev, curr) => prev + curr);
+};
+
+logMsg(total(10, 2, 3));
+
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
+
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break;
+  }
+};
+
+const numberOrString = (value: number | string): string => {
+  if (typeof value === 'string') return 'string';
+  if (typeof value === 'number') return 'number';
+  return createError('This could never happen');
+};
